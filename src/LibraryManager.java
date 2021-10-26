b5ebf43fb396b5a919863ed4eb500754d4183a79
Salim Hadhri <salim.hadhri@talan.com>
2021-10-26 07:46:09 +0100

    Connexion établie et 1er traitement: 	afficherLivres()

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -1,34 +1,51 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 
 	
 
 		public class LibraryManager { //Il en va de soit que plus tard cette méthode sera améliorée.
-		    private static String url = "jdbc:postgresql://localhost:5432/library";
-		    private static String driverName = null;
-		    private static String username = "postgres";
-		    private static String password = "DkPB7D+";
-		    private static Connection con;
-		    private static String urlstring;
 
-		    public static Connection getConnection() {
-		        try {
-		            Class.forName(driverName);
-		            try {
-		                con = DriverManager.getConnection(url,username, password);
-		            } catch (SQLException ex) {
-		                // log an exception. fro example:
-		                System.out.println("Failed to create the database connection.");
-		            }
-		        } catch (ClassNotFoundException ex) {
-		            // log an exception. for example:
-		            System.out.println("Driver not found.");
-		        }
-		        return con;
-		    }
+
+			Connection  con = ConnectionManager.getConnection();
+		    ResultSet résultats = null;
+		    String requete = "";
+		    ResultSetMetaData rsmd;
+			 
+		    private static void arret(String message) {
+		        System.err.println(message);
+		        System.exit(99);
+		     }
 		    
+			public void afficherLivres() {				
+
+				requete = "SELECT * FROM library.book ; ";
+				
+				try {
+			         Statement stmt = con.createStatement();
+			         résultats = stmt.executeQuery(requete);
+					 boolean encore = résultats.next();
+					   while (encore) {
+						   System.out.println("*********** new book ********");
+						   System.out.println(résultats.getString("title")+ " \n"+ résultats.getString("author") + " \n" +  résultats.getString("editor") +  "\n" +  résultats.getString("summary")
+						  +  "\n" +  résultats.getInt("page_nb")); 
+						   
+						   encore = résultats.next();
+					   }
+					   résultats.close();
+					} catch (SQLException e) {
+						arret("Anomalie lors de l'execution de la requête") ;
+					}
+			}
+			public LibraryManager() {
+			}
+			
+						
 		}
 		
 
---
+++ src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		LibraryManager librManag = new LibraryManager();
+		
+		librManag.afficherLivres(); 
+
+	}
+
+}
